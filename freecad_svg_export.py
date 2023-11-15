afb785edd14b33932b025898c710ef26d8886768
Tinmar <ElTinmar@users.noreply.github.com>
2023-11-15 12:14:55 +0100

    Update freecad_svg_export.py

--- freecad_svg_export.py
+++ freecad_svg_export.py
@@ -2,12 +2,6 @@ import FreeCAD as App
 import importSVG
 import os
 
-'''
-This will export properly only sketches in the XY plane.
-Part design menu > Reorient sketch. If the sketch was previously mapped 
-to a face FreeCAD will prompt to to remove from support
-'''
-
 # FreeCAD.ActiveDocument.getObjectsByLabel('AngleBracketLED')
 sketches_to_export = FreeCAD.ActiveDocument.findObjects(Type='Sketcher::SketchObject')
 
@@ -16,4 +10,18 @@ path = os.path.dirname(docname)
 
 for sketch in sketches_to_export:
     outname = os.path.join(path, sketch.Label + ".svg")
+    
+    # save sketch placement
+    placement = sketch.Placement
+    
+    # Align all sketches to XY plane
+    sketch.Placement = App.Placement(
+        App.Vector(0.000000, 0.000000, 0.000000), 
+        App.Rotation(0.000000, 0.000000, 0.000000, 1.000000)
+    )
+    
+    # Export SVG
     importSVG.export([sketch], outname)
+    
+    # restore placement
+    sketch.Placement = placement
