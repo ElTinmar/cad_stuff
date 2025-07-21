23fc5944b438fb2f12532a1b77662fb35705ad41
Tinmar <ElTinmar@users.noreply.github.com>
2025-07-21 15:03:53 +0200

    red hairline

--- freecad_svg_export.py
+++ freecad_svg_export.py
@@ -2,6 +2,23 @@ import FreeCAD as App
 import importSVG
 import os
 from datetime import datetime
+import re
+
+def make_svg_hairline_red(filepath):
+    with open(filepath, "r", encoding="utf-8") as f:
+        content = f.read()
+
+    # Replace existing style attribute in <path ...> tags
+    content = re.sub(
+        r'(<path\b[^>]*?)\sstyle="[^"]*?"',
+        r'\1 style="stroke:#ff0000;stroke-width:0.02;stroke-dasharray:none;fill:none;fill-opacity:1;fill-rule:evenodd"',
+        content,
+        flags=re.IGNORECASE
+    )
+
+    with open(filepath, "w", encoding="utf-8") as f:
+        f.write(content)
+
 
 # FreeCAD.ActiveDocument.getObjectsByLabel('AngleBracketLED')
 sketches_to_export = FreeCAD.ActiveDocument.findObjects(Type='Sketcher::SketchObject')
@@ -23,6 +40,7 @@ for sketch in sketches_to_export:
     
     # Export SVG
     importSVG.export([sketch], outname)
+    make_svg_hairline_red(outname)
     
     # restore placement
     sketch.Placement = placement
